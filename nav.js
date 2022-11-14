5fe472dc21cdde0e781e7a83963b20d54cf0d5d8
Jan <janalbrecht05@gmail.com>
2022-11-14 23:33:33 +0100

    adding the navbar with responsive width script

---
+++ nav.js
@@ -0,0 +1,10 @@
+function opennav(){
+    document.getElementById("top").style.display = "block";
+    document.getElementById("hamburger1").style.display = "none";
+    document.getElementById("hamburger2").style.display = "block";
+}
+function closenav(){
+    document.getElementById("top").style.display = "none";
+    document.getElementById("hamburger1").style.display = "block";
+    document.getElementById("hamburger2").style.display = "none";
+}
